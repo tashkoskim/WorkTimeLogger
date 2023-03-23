eb3bb2ca8345f4ef32b84a5a3dd72e8ddeca98cf
Martin Tashkoski <tashkoskim@yahoo.com>
2023-03-23 19:18:13 +0100

    Add project files.

---
+++ WorkTimeLogger/Constants/GeneralConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTimeLogger.Constants
+{
+    public static class GeneralConstants
+    {
+        public static string logsFolder = $"{Path.GetDirectoryName(Application.ExecutablePath)}\\Logs\\";
+        public static string bckgrImg_btnMeeting1 = $"{Path.GetDirectoryName(Application.ExecutablePath)}\\Images\\btn_Meeting1.png";
+        public static string bckgrImg_btnMeeting2 = $"{Path.GetDirectoryName(Application.ExecutablePath)}\\Images\\btn_Meeting2.png";
+        public static string bckgrImg_btnCoding1 = $"{Path.GetDirectoryName(Application.ExecutablePath)}\\Images\\btn_Coding1.png";
+        public static string bckgrImg_btnCoding2 = $"{Path.GetDirectoryName(Application.ExecutablePath)}\\Images\\btn_Coding2.png";
+    }
+}
---
+++ WorkTimeLogger/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WorkTimeLogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileLocationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Meeting = new System.Windows.Forms.Button();
+            this.btn_Coding = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.panelHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Work Time Logger";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.openFileLocationToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(216, 70);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // openFileLocationToolStripMenuItem
+            // 
+            this.openFileLocationToolStripMenuItem.Name = "openFileLocationToolStripMenuItem";
+            this.openFileLocationToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.openFileLocationToolStripMenuItem.Text = "Open folder in file explorer";
+            this.openFileLocationToolStripMenuItem.Click += new System.EventHandler(this.openFileLocationToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panelHeader.Controls.Add(this.label1);
+            this.panelHeader.Location = new System.Drawing.Point(2, 2);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(457, 30);
+            this.panelHeader.TabIndex = 0;
+            this.panelHeader.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(4, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "__:__ - __:__";
+            // 
+            // btn_Meeting
+            // 
+            this.btn_Meeting.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Meeting.BackgroundImage")));
+            this.btn_Meeting.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Meeting.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Meeting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Meeting.Location = new System.Drawing.Point(108, 41);
+            this.btn_Meeting.Name = "btn_Meeting";
+            this.btn_Meeting.Size = new System.Drawing.Size(60, 60);
+            this.btn_Meeting.TabIndex = 0;
+            this.btn_Meeting.TabStop = false;
+            this.btn_Meeting.UseVisualStyleBackColor = true;
+            this.btn_Meeting.MouseEnter += new System.EventHandler(this.btn_Meeting_MouseEnter);
+            this.btn_Meeting.MouseLeave += new System.EventHandler(this.btn_Meeting_MouseLeave);
+            // 
+            // btn_Coding
+            // 
+            this.btn_Coding.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Coding.BackgroundImage")));
+            this.btn_Coding.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Coding.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Coding.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Coding.Location = new System.Drawing.Point(12, 41);
+            this.btn_Coding.Name = "btn_Coding";
+            this.btn_Coding.Size = new System.Drawing.Size(60, 60);
+            this.btn_Coding.TabIndex = 0;
+            this.btn_Coding.TabStop = false;
+            this.btn_Coding.UseVisualStyleBackColor = true;
+            this.btn_Coding.MouseEnter += new System.EventHandler(this.btn_Coding_MouseEnter);
+            this.btn_Coding.MouseLeave += new System.EventHandler(this.btn_Coding_MouseLeave);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 107);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(241, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(461, 187);
+            this.ControlBox = false;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_Coding);
+            this.Controls.Add(this.btn_Meeting);
+            this.Controls.Add(this.panelHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Time Logger";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NotifyIcon notifyIcon1;
+        private Panel panelHeader;
+        private Label label1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem showToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem openFileLocationToolStripMenuItem;
+        private Button btn_Meeting;
+        private Button btn_Coding;
+        private ComboBox comboBox1;
+        private ToolTip toolTip1;
+    }
+}
